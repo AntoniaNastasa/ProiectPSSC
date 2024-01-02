5a356a2e024919c48368327c274abcd51fa3cd79
AntoniaNastasa <80025892+AntoniaNastasa@users.noreply.github.com>
2024-01-02 18:12:26 +0200

    Update Comanda.cs

--- EShop/EShop/Data/Models/Comanda.cs
+++ EShop/EShop/Data/Models/Comanda.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EShop.Data.Models
 {
-  public record Comanda
+  public class Comanda
     {
         public int comanda_id { get; set; }
         public int client_id { get; set; }
