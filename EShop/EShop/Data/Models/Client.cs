691f5e6e64e1c90a06aed1c56118f6c53be040a8
AntoniaNastasa <80025892+AntoniaNastasa@users.noreply.github.com>
2024-01-02 18:05:03 +0200

    Update Client.cs

--- EShop/EShop/Data/Models/Client.cs
+++ EShop/EShop/Data/Models/Client.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EShop.Data.Models
 {
-    internal class Client
+    public record Client
     {
         public int client_id { get; set; }
         public string nume { get; set; }
