4dbad8c676ae5faa2d634670222f31232d1e2b29
AntoniaNastasa <80025892+AntoniaNastasa@users.noreply.github.com>
2024-01-02 16:41:39 +0200

    Add files via upload

---
+++ EShop/EShop/Data/Models/Produs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Data.Models
+{
+    internal class Produs
+    {
+        public int produs_id { get; set; }
+        public string nume { get; set; }
+        public decimal pret { get; set; }
+    }
+}
